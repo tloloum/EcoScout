31aaf4955dd867fcc7071407254bbc240ad065c2
Thomas Tuilier <ttuilier@enseirb-matmeca.fr>
2024-02-26 18:15:09 +0100

    Début de mise en forme du login

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -3,7 +3,29 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <p>testLogin</p>
+      <h1>Login</h1>
+
+      <section id="form">
+        <h2>Se connecter</h2>
+        <form action="">
+          <label for="login-email">Adresse mail : </label>
+          <input
+            type="text"
+            id="login-email"
+            placeholder="Entrez votre adresse mail"
+          />
+          <br />
+          <label for="login-mot_de_passe">Mot de passe : </label>
+          <input
+            type="text"
+            id="login-mot_de_passe"
+            placeholder="Entrez votre mot de passe"
+          />
+          <br />
+          <br />
+          <input type="submit" value="Valider" />
+        </form>
+      </section>
     </div>
   );
 };
