6d2f8698d73100a36b4240949dc2b86c47e0c4f7
Thomas Tuilier <ttuilier@enseirb-matmeca.fr>
2024-02-26 17:47:11 +0100

    Commentaire de App.js en moins

--- front/src/App.js
+++ front/src/App.js
@@ -8,35 +8,6 @@ import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import { hasAuthenticated } from "./services/AuthApi";
 
-// const TestProfile = ({ isAuthenticated }) => {
-//   if (!isAuthenticated) {
-//     return <Navigate to="/login" />;
-//   } else {
-//     return <Navigate to="/profile" />;
-//   }
-// };
-
-// const App = () => {
-//   const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
-//   return (
-//     <Auth.Provider value={{ isAuthenticated }}>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route exact path="/" element={<Home />} />
-//           <Route path="*" element={<Error404 />} />
-//           <Route path="/register" element={<Register />} />
-//           <Route path="/login" element={<Login />} />
-
-//           <Route
-//             path="/profile"
-//             element={isAuthenticated ? <MyProfile /> : <Navigate to="/login" />}
-//           />
-//         </Routes>
-//       </BrowserRouter>
-//     </Auth.Provider>
-//   );
-// };
-
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
 
