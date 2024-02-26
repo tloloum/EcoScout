423776a48f2d9cd8cc8c62aa6bda90deea7c5752
Thomas Tuilier <ttuilier@enseirb-matmeca.fr>
2024-02-26 17:46:17 +0100

    modification de la securité des pages avec le login

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./contexts/Auth";
 import Error404 from "./pages/Error404";
 import Home from "./pages/Home";
@@ -8,17 +8,50 @@ import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import { hasAuthenticated } from "./services/AuthApi";
 
+// const TestProfile = ({ isAuthenticated }) => {
+//   if (!isAuthenticated) {
+//     return <Navigate to="/login" />;
+//   } else {
+//     return <Navigate to="/profile" />;
+//   }
+// };
+
+// const App = () => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
+//   return (
+//     <Auth.Provider value={{ isAuthenticated }}>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route exact path="/" element={<Home />} />
+//           <Route path="*" element={<Error404 />} />
+//           <Route path="/register" element={<Register />} />
+//           <Route path="/login" element={<Login />} />
+
+//           <Route
+//             path="/profile"
+//             element={isAuthenticated ? <MyProfile /> : <Navigate to="/login" />}
+//           />
+//         </Routes>
+//       </BrowserRouter>
+//     </Auth.Provider>
+//   );
+// };
+
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
+
   return (
     <Auth.Provider value={{ isAuthenticated }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="*" element={<Error404 />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/profile"
+            element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
+          />
         </Routes>
       </BrowserRouter>
     </Auth.Provider>
--- front/src/contexts/Auth.js
+++ front/src/contexts/Auth.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { hasAuthenticated } from "../services/AuthApi";
 
 export default React.createContext({
-  isAuthenticated: false,
+  isAuthenticated: hasAuthenticated(),
 });
