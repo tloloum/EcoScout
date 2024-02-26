8d8773e6ed109b6751895fa490368a3651d71fc2
Thomas Tuilier <ttuilier@enseirb-matmeca.fr>
2024-02-26 16:57:23 +0100

    Modification du nom du fichier Profile

---
+++ front/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Profile = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Profil</h1>
+    </div>
+  );
+};
+
+export default Profile;
