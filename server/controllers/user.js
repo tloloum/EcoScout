1f51c606fedfc98de01694518e08d150b766a986
Arthur <ahermitte@enseirb-matmeca.fr>
2024-02-29 21:32:41 +0100

    user login premier jet

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,42 +1,52 @@
 // Ce fichier contient les middlewares qui gèrent les requêtes vers la route /user
-const bcrypt = require('bcrypt');
-const connection = require('../connection')
+const bcrypt = require("bcrypt");
+const connection = require("../connection");
+
 
 exports.register = (req, res, next) => {
-    console.log('Register request')
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            console.log('hash effectué')
-            console.log(hash)
-            const register_query = `INSERT INTO Utilisateurs (id_user, username, mdp) VALUES ('0', '${req.body.username}', '${hash}')`;
-            connection.query(register_query, (error) => {
-                if (error) {
-                    console.log('query error')
-                    throw error;
-                } else {
-                    console.log('user ok')
-                    res.status(201).json({ message: 'User inserted successfully' });
-                }
-            });
-        })
-        .catch(error => {
-            console.log("jsp ski spas")
-            res.status(500).json({error})
-        })
+  console.log("Register request");
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      console.log(hash);
+      const register_query = `INSERT INTO Utilisateurs (id_user, username, mdp) VALUES ('0', '${req.body.username}', '${hash}')`;
+      connection.query(register_query, (error) => {
+        if (error) {
+          throw error;
+        } else {
+          res.status(201).json({ message: "User inserted successfully" });
+        }
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
 };
 
 exports.login = (req, res, next) => {
-
+  console.log("Login request");
+  const username = req.body.username;
+  const login_query = `SELECT id_user, mdp FROM Utilisateurs WHERE username = '${username}'`;
+  connection.query(login_query, (error, rows) => {
+    if (error) throw error;
+    else {
+      bcrypt
+        .compare(req.body.password, rows[0].mdp)
+        .then((is_valid) => {
+            if (!is_valid) res.status(401).json({ message: "Identifiant ou mot de passe incorrect" });
+            else {
+                res.status(200).json({message: "Connexion réussie !"})
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({error});
+        })
+    }
+  });
 };
 
-exports.getUser = (req, res, next) => {
+exports.getUser = (req, res, next) => {};
 
-};
+exports.updateUser = (req, res, next) => {};
 
-exports.updateUser = (req, res, next) => {
-
-};
-
-exports.deleteUser = (req, res, next) => {
-
-};
+exports.deleteUser = (req, res, next) => {};
