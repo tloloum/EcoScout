fd3217b37540e23cbe04f72ab4a49e4a4e3f545d
Arthur <ahermitte@enseirb-matmeca.fr>
2024-03-04 13:05:33 +0100

    user factorisation de code et check si utilisateur existe deja

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,53 +3,86 @@ const bcrypt = require("bcrypt");
 const connection = require("../connection");
 const jwt = require("jsonwebtoken");
 
-exports.register = (req, res, next) => {
-  console.log("Register request");
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const register_query = `INSERT INTO Utilisateurs (id_user, username, mdp) VALUES ('0', '${req.body.username}', '${hash}')`;
-      connection.query(register_query, (error) => {
+send_query_insert = (msg, res, status_nb, status_msg) => {
+  connection.query(msg, (error) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(status_nb).json({ message: status_msg });
+    }
+  });
+};
+
+send_query_select = async (msg) => {
+  try {
+    const rows = await new Promise((resolve, reject) => {
+      connection.query(msg, (error, rows) => {
         if (error) {
-          throw error;
+          reject(error);
         } else {
-          res.status(201).json({ message: "User inserted successfully" });
+          resolve(rows);
         }
       });
+    });
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+check_if_exists = async (table, field, name) => {
+  const query = `SELECT COUNT(*) AS count FROM ${table} WHERE ${field}='${name}'`;
+  let u = await send_query_select(query);
+  return u[0].count;
+};
+
+exports.register = async (req, res, next) => {
+  console.log("Register request");
+  if (await check_if_exists("Utilisateurs", "username", req.body.username))
+    return res.status(400).json({ message: "Username already taken" });
+  else {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const register_query = `INSERT INTO Utilisateurs (id_user, username, mdp) VALUES ('0', '${req.body.username}', '${hash}')`;
+        send_query_insert(
+          register_query,
+          res,
+          201,
+          "User inserted successfully"
+        );
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  console.log("Login request");
+  const username = req.body.username;
+  const password = req.body.password;
+  const login_query = `SELECT id_user, mdp FROM Utilisateurs WHERE username = '${username}'`;
+  const rows = await send_query_select(login_query);
+  bcrypt
+    .compare(password, rows[0].mdp)
+    .then((is_valid) => {
+      if (!is_valid)
+        res
+          .status(401)
+          .json({ message: "Identifiant ou mot de passe incorrect" });
+      else {
+        res.status(200).json({
+          userId: rows[0].id_user,
+          token: jwt.sign({ userId: rows[0].id_user }, "RANDOM_TOKEN", {
+            expiresIn: "24h",
+          }),
+        });
+      }
     })
     .catch((error) => {
       res.status(500).json({ error });
     });
-};
-
-exports.login = (req, res, next) => {
-  console.log("Login request");
-  const username = req.body.username;
-  const login_query = `SELECT id_user, mdp FROM Utilisateurs WHERE username = '${username}'`;
-  connection.query(login_query, (error, rows) => {
-    if (error) throw error;
-    else {
-      bcrypt
-        .compare(req.body.password, rows[0].mdp)
-        .then((is_valid) => {
-          if (!is_valid)
-            res
-              .status(401)
-              .json({ message: "Identifiant ou mot de passe incorrect" });
-          else {
-            res.status(200).json({
-              userId: rows[0].id_user,
-              token: jwt.sign({ userId: rows[0].id_user }, "RANDOM_TOKEN", {
-                expiresIn: "24h",
-              }),
-            });
-          }
-        })
-        .catch((error) => {
-          res.status(500).json({ error });
-        });
-    }
-  });
 };
 
 exports.createAdherent = (req, res, next) => {
@@ -61,13 +94,7 @@ exports.createAdherent = (req, res, next) => {
   }
   console.log(nom_ad, prenom_ad, mail_ad, userId);
   const register_query = `INSERT INTO Adherents (id_adherent, nom_ad, prenom_ad, mail_ad, id_user) VALUES ('0', '${nom_ad}', '${prenom_ad}', '${mail_ad}', '${userId}')`;
-  connection.query(register_query, (error) => {
-    if (error) {
-      throw error;
-    } else {
-      res.status(201).json({ message: "Adherent inserted successfully" });
-    }
-  });
+  send_query_insert(register_query, res, 201, "Adherent inserted successfully");
 };
 
 exports.selectAdherent = (req, res, next) => {
