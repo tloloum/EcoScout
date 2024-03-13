685d27779f7f50ecf30fcac6725b4e5942c22fd0
HERMITTE Arthur <Arthur.Hermitte@bordeaux-inp.fr>
2024-03-13 05:30:36 +0100

    back: route impact/calcul peut calculer l'impact d'un trajet en voiture

--- server/controllers/impact.js
+++ server/controllers/impact.js
@@ -2,63 +2,44 @@
 // URL de la documentation de l'api
 // https://data.ademe.fr/data-fair/api/v1/datasets/base-carboner/api-docs.json
 
-// const res = request(app)
-//   .get(
-//     "https://data.ademe.fr/data-fair/api/v1/datasets/base-carboner/lines?select=Type_Ligne,Structure&q=voiture"
-//   )
-//   .expect(201);
+let stored_results;
 
-var XMLHttpRequest = require("xhr2"); //Module pour faire des requêtes d'api
-
-const send_request = (type, url, callback) => {
-  //type = "GET" | "POST" | "DELETE" | etc
-  const xhr = new XMLHttpRequest();
-  xhr.open(type, url);
-  xhr.send();
-  xhr.responseType = "json";
-  xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      callback(null, xhr.response);
+exports.getImpact = async (req, res, next) => {
+  if (
+    req.body.type_moteur === undefined ||
+    req.body.distance === undefined ||
+    req.body.nombre_passagers === undefined ||
+    req.body.vehicule === undefined
+  ) {
+    return res.status(400).json({ message: "Not enough data provided" });
+  } else {
+    const type_vehicule = req.body.type_vehicule;
+    if (stored_results === undefined) {
+      const data = await fetch(
+        "https://data.ademe.fr/data-fair/api/v1/datasets/base-carboner/lines?select=*&q=Voiture"
+      );
+      const dataJson = await data.json();
+      console.log(dataJson.results);
+      stored_results = dataJson.results;
+    }
+    // Les types de motorisation: "E85", "essence", "gazole", "GNV", "GPL", "moyenne"
+    const impact = stored_results
+      .filter((element) => element.Nom_base_français === req.body.vehicule)
+      .filter(
+        (element) =>
+          element.Nom_attribut_français ===
+          `Motorisation ${req.body.type_moteur}`
+      );
+    if (impact.length === 0) {
+      return res.status(400).json({ message: "No data" });
     } else {
-      callback(`Error: ${xhr.status}, ${xhr.response}`, null);
-    }
-  };
-};
-
-const response_to_tab = (res) => {
-  const l = res.length;
-  for (i = 0; i < l; i++) {
-    console.log("==================");
-    const obj = res[i];
-    const keys = Object.keys(obj);
-    for (j = 0; j < keys.length; j++) {
-      console.log(`${keys[j]}: ${obj[keys[j]]}`);
+      const conso =
+        req.body.distance *
+        impact[0].Total_poste_non_décomposé *
+        req.body.nombre_passagers;
+      return res
+        .status(200)
+        .json({ impact_data: impact, consommation: conso, unité: "kgCO2e" });
     }
   }
-  console.log("==================");
 };
-
-const print_response = (error, response) => {
-  if (error) console.log(error);
-  else {
-    // response_to_tab(response);
-    console.log(response)
-  }
-};
-
-const get_api = (route) => {
-  const url = `https://data.ademe.fr/data-fair/api/v1/datasets/base-carboner/${route}`;
-  send_request("GET", url, print_response);
-};
-
-const get_api_doc = () => get_api("api-docs.json");
-
-const get_request = (champs, valeurs) => {
-  //Attention à ne pas mettre d'espace entre les champs, ex: "champ1,champ2" OK mais "champ1, champ2" NOK
-  const url = `https://data.ademe.fr/data-fair/api/v1/datasets/base-carboner/lines?select=${champs}&q=${valeurs}`;
-  send_request("GET", url, print_response);
-};
-
-// get_request("Nom_base_français,Type_Ligne,Type_poste,CO2f", "voiture");
-// get_request("*", "Voiture")
-// get_api("schema");
--- server/routes/impact.js
+++ server/routes/impact.js
@@ -4,9 +4,11 @@ const express = require("express");
 const router = express.Router();
 const impactCtrl = require('../controllers/impact')
 
-router.use((req, res, next) => {
-  console.log("Requete de calcul d'impact");
-  res.send("TON TAU DE CARBONE EST DE 1000 TONNES C BEAUCOUP");
-});
+router.post("/calcul", impactCtrl.getImpact);
+
+// router.use((req, res, next) => {
+//   console.log("Requete de calcul d'impact");
+//   res.send("TON TAU DE CARBONE EST DE 1000 TONNES C BEAUCOUP");
+// });
 
 module.exports = router;
