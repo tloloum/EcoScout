00e6b5f5eb0e944cb6471aefbaf7c9d47533f857
Arthur <arthurdurand3@gmail.com>
2024-04-04 13:44:09 +0200

    j'ai pas eu le time de finir struct

--- server/controllers/struct.js
+++ server/controllers/struct.js
@@ -19,6 +19,28 @@ exports.createStruct = async (req, res, next) => {
   }
 };
 
+exports.getStructsFromUser = async (req, res, next) => {
+  const userId = parseInt(req.params.userId, 10);
+  if (userId !== req.auth.userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const select_query = `SELECT id_structur, nom_structure, date_creation, id_structur_mere FROM structur WHERE id_owner = '${userId}'`;
+  utils
+    .send_query_select(select_query)
+    .then((rows) => {
+      res.status(200).json(rows);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
+exports.loginStruct = async (req, res, next) => {
+  if (req.body.structureId == undefined) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+};
+
 exports.getStruct = async (req, res, next) => {
   const id_structur = parseInt(req.params.structureId, 10);
   const query = `SELECT nom_structur, date_creation, id_structur_mere FROM Structur WHERE id_structur = '${id_structur}' `; //On renvoie l'id de la structure mere?? la structure mere en soit?
@@ -69,15 +91,16 @@ exports.addMember = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   } else {
     const query = `INSERT INTO Participants_Struct (id_p_struct, date_join, id_structur, id_adherent) VALUES ( '0' , GETDATE(), '${structureId}' , '${adherentId}' )`;
-    utils.send_query_insert(
-      query,
-      res,
-      200,
-      "Member added to structure successfully"
-    )
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    utils
+      .send_query_insert(
+        query,
+        res,
+        200,
+        "Member added to structure successfully"
+      )
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 };
 
@@ -88,53 +111,55 @@ exports.removeMember = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   } else {
     const query = `DELETE FROM Participants_Struct WHERE id_structure=${structureId} AND id_adherent=${adherentId}`;
-    utils.send_query_insert(
-      query,
-      res,
-      200,
-      "Member deleted from structure successfully"
-    )
-    .catch((error) => {
-      res.status(500).json({ error });
-    });;
+    utils
+      .send_query_insert(
+        query,
+        res,
+        200,
+        "Member deleted from structure successfully"
+      )
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 };
 
 exports.joinStruct = (req, res, next) => {
   const userId = parseInt(req.params.userId, 10); //vraiment utile ici??
   const structureId = parseInt(req.params.structureId, 10);
-  if (structureId !== req.auth.structureId) { 
+  if (structureId !== req.auth.structureId) {
     return res.status(401).json({ message: "Unauthorized" });
   } else {
     const query = `INSERT INTO Participants_Struct (id_p_struct, date_join, id_structur, id_adherent) VALUES ( '0' , GETDATE(), '${structureId}' , '${adherentId}' )`;
-    utils.send_query_insert(
-      query,
-      res,
-      200,
-      "Member added to structure from link successfully"
-    )
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    utils
+      .send_query_insert(
+        query,
+        res,
+        200,
+        "Member added to structure from link successfully"
+      )
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 };
 
 exports.joinHierarchy = (req, res, next) => {
   const structureId = parseInt(req.params.structureId, 10);
   const structureIdMere = req.body.structureId;
-  if (structureId !== req.auth.structureId) { 
+  if (structureId !== req.auth.structureId) {
     return res.status(401).json({ message: "Unauthorized" });
-  }
-  else {
+  } else {
     const query = `UPDATE Structur SET id_structur_mere=${structureIdMere} WHERE id ) ${structureId}`;
-    utils.send_query_insert(
-      query,
-      res,
-      200,
-      "Structure joined another successfully"
-    )
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    utils
+      .send_query_insert(
+        query,
+        res,
+        200,
+        "Structure joined another successfully"
+      )
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   }
 };
