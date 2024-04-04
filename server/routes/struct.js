d7a00df9b991b1af9ff66955ad8dbaaa1b1aa7b5
Theophane Loloum <tloloum@bordeaux-inp.fr>
2024-04-04 12:13:47 +0200

    struct routes

--- server/routes/struct.js
+++ server/routes/struct.js
@@ -1,10 +1,10 @@
 // Ce fichier contient les routes concernant les structures
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const structCtrl = require('../controllers/struct');
-const auth = require('../middleware/auth_struct');
-const auth_struct = require('../middleware/auth_struct');
+const structCtrl = require("../controllers/struct");
+const auth = require("../middleware/auth_struct");
+const auth_struct = require("../middleware/auth_struct");
 
 /*
 1. **POST** `/structures` - Pour créer une nouvelle structure.
@@ -17,18 +17,25 @@ const auth_struct = require('../middleware/auth_struct');
 8. **POST** `/structures/{structureId}/hierarchy` - Pour faire rejoindre une structure à une autre (gestion de la hiérarchie).
 */
 
-router.post('/create', auth_struct, structCtrl.createStruct);
-router.get('/:structureId', auth_struct, structCtrl.getStruct);
-router.put('/:structureId', auth_struct, structCtrl.updateStruct);
-router.delete('/:structureId', auth_struct, structCtrl.deleteStruct);
-router.post('/:structureId/members', auth_struct, structCtrl.addMember);
-router.delete('/:structureId/members/:memberId', auth_struct, structCtrl.removeMember);
-router.post('/:structureId/join', auth_struct, structCtrl.joinStruct);
-router.post('/:structureId/hierarchy', auth_struct, structCtrl.joinHierarchy);
+router.post("/create", auth, structCtrl.createStruct);
+router.post("loginstruct", auth, structCtrl.loginStruct); // renvoie le nouveau token
+router.get("/:userId/structures", auth, structCtrl.getStructsFromUser);
+
+router.get("/:structureId", auth_struct, structCtrl.getStruct);
+router.put("/:structureId", auth_struct, structCtrl.updateStruct);
+router.delete("/:structureId", auth_struct, structCtrl.deleteStruct);
+router.post("/:structureId/members", auth_struct, structCtrl.addMember);
+router.delete(
+  "/:structureId/members/:memberId",
+  auth_struct,
+  structCtrl.removeMember
+);
+router.post("/:structureId/join", auth_struct, structCtrl.joinStruct);
+router.post("/:structureId/hierarchy", auth_struct, structCtrl.joinHierarchy);
 
 router.use((req, res, next) => {
-    console.log("Requete de structure");
-    res.send('Structure');
+  console.log("Requete de structure");
+  res.send("Structure");
 });
 
 module.exports = router;
