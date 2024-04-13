5d7df4087999ad802e34ca253ca32c1fc8bae0fb
Thomas Tuilier <ttuilier@enseirb-matmeca.fr>
2024-04-13 20:19:09 +0200

    Adding creation of structure

--- front/src/components/CreateStructure.js
+++ front/src/components/CreateStructure.js
@@ -1,36 +1,100 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useContext, useState } from "react";
 import { RiCloseLine } from "react-icons/ri";
+import { AuthContext } from "../contexts/Auth";
+import { ServerContext } from "../contexts/Server";
 
 const CreateStructure = () => {
-  const defaultFormData = {
-    name: "",
-    region: "",
-    date: "",
+  const { getServerAddress } = useContext(ServerContext);
+
+  const { myToken } = useContext(AuthContext);
+
+  const [nameStructure, setNameStructure] = useState("");
+  const [regionStructure, setRegionStructure] = useState("");
+  const [dateStructure, setDateStructure] = useState("");
+
+  const handleNameStructureChange = (event) => {
+    setNameStructure(event.target.value);
   };
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    defaultValues: defaultFormData,
-  });
-  console.log(errors);
+
+  const handleRegionStructureChange = (event) => {
+    setRegionStructure(event.target.value);
+  };
+
+  const handleDateStructureChange = (event) => {
+    setDateStructure(event.target.value);
+  };
+
+  // const defaultFormData = {
+  //   name: "",
+  //   region: "",
+  //   date: "",
+  // };
+
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   reset,
+  //   formState: { errors },
+  // } = useForm({
+  //   defaultValues: defaultFormData,
+  // });
+
+  // console.log(errors);
   const [showForm, setShowForm] = useState(false);
+
   const handleClick = () => {
     setShowForm(true);
   };
 
-  const handleUndo = () => {
-    reset();
-  };
+  // const handleUndo = () => {
+  //   reset();
+  // };
 
-  const onSubmit = (data) => {
-    console.log(data);
-    handleUndo();
-    setShowForm(false);
-  };
+  async function structureSubmit(event) {
+    event.preventDefault();
+    const serverAddress = getServerAddress();
+    console.log(serverAddress + "structures");
+
+    // Creation du compte :
+    const resultStructure = await fetch(serverAddress + "structures/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + myToken,
+      },
+      body: JSON.stringify({
+        nom_structure: nameStructure,
+      }),
+    });
+
+    if (resultStructure.status !== 201) {
+      console.log("Erreur lors de la création de la structure");
+      return;
+    } else {
+      console.log(serverAddress + "structures");
+      const resultStructureContent = await resultStructure.json();
+      console.log(resultStructureContent);
+    }
+
+    //   if (resultToken.status !== 200) {
+    //     setErreurMessage("Identifiant ou mot de passe incorrect");
+    //     return;
+    //   } else {
+    //     const resultTokenContent = await resultToken.json();
+    //     setUserId(resultTokenContent.userId);
+    //     setToken(resultTokenContent.token);
+    //     login(email, password);
+    //     navigate("/chooseadherant");
+    //   }
+    // }
+  }
+
+  // const onSubmit = (data) => {
+  //   console.log(data);
+  //   handleUndo();
+  //   setShowForm(false);
+  // };
+
   return (
     <main>
       <button className="primaryBtn" onClick={handleClick}>
@@ -47,58 +111,87 @@ const CreateStructure = () => {
               <button
                 className="closeBtn"
                 onClick={() => {
-                  handleUndo();
+                  //handleUndo();
                   setShowForm(false);
                 }}
               >
                 <RiCloseLine style={{ marginBottom: "-3px" }} />
               </button>
               <div className="modalContent">
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={structureSubmit}>
+                  {/* {handleSubmit(onSubmit)}> */}
                   <label>
                     Nom de la structure:
                     <input
-                      {...register("name", {
-                        required: "Ce champ est obligatoire",
-                      })}
+                      // {...register("name", {
+                      //   required: "Ce champ est obligatoire",
+                      // })}
                       type="text"
                       name="name"
                       placeholder="insérer le nom de la structure"
+                      value={nameStructure}
+                      onChange={handleNameStructureChange}
                     />
-                    {errors.region && (
+                    {/* {errors.region && (
                       <span className="errorMsg">{errors.name.message}</span>
-                    )}
+                    )} */}
                   </label>
                   {/* <p className="errorMsg">{errors.name?.message}</p> */}
                   <br />
                   <label>
                     La région:
                     <input
-                      {...register("region", {
-                        required: " Ce champ est obligatoire",
-                      })}
+                      // {...register("region", {
+                      //   required: " Ce champ est obligatoire",
+                      // })}
                       type="region"
                       name="region"
                       placeholder="insérer la région"
+                      value={regionStructure}
+                      onChange={handleRegionStructureChange}
                     />
-                    {errors.region && (
+                    {/* {errors.region && (
                       <span className="errorMsg">{errors.region.message}</span>
-                    )}
+                    )} */}
                   </label>
                   {/* <p className="errorMsg">{errors.name?.message}</p> */}
                   <br />
                   <label>
                     Date de formation:
-                    <input {...register("date")} type="date" name="date" />
+                    <input
+                      // {...register("date")}
+                      type="date"
+                      name="date"
+                      placeholder="insérer la date de création"
+                      value={dateStructure}
+                      onChange={handleDateStructureChange}
+                    />
                   </label>
                   <br />
+                  <div className="modalActions">
+                    <div className="actionsContainer">
+                      <button
+                        type="submit"
+                        className="submitBtn"
+                        // onClick={structureSubmit(handleSubmit(onSubmit))}
+                      >
+                        Soumettre
+                      </button>
+                      {/* <button
+                        className="undoBtn"
+                        onClick={handleUndo}
+                      >
+                        Annuler
+                      </button> */}
+                    </div>
+                  </div>
                 </form>
               </div>
-              <div className="modalActions">
+              {/* <div className="modalActions">
                 <div className="actionsContainer">
                   <button
                     className="submitBtn"
-                    onClick={handleSubmit(onSubmit)}
+                    onClick={structureSubmit(handleSubmit(onSubmit))}
                   >
                     Soumettre
                   </button>
@@ -106,7 +199,7 @@ const CreateStructure = () => {
                     Annuler
                   </button>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </>
