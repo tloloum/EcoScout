259b7fae43deb4c43433666507b21228ceecb3f8
Théophane Loloum <theophane.loloum@gmail.com>
2024-04-22 14:03:34 +0200

    add routes + controllers evenements (pas testé encore)

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,21 +1,158 @@
-
+const connection = require("../bdd/utils/connection.js");
+const utils = require("../bdd/utils/utils");
 
 exports.createEvent = (req, res, next) => {
-
+  const structId = req.auth.structureId;
+  const nom_evenement = req.body.nom_evenement;
+  const date_debut = req.body.date_debut;
+  const lieu = req.body.lieu;
+  const descr = req.body.descr;
+  const duree_evenement = req.body.duree_evenement;
+  if (!nom_evenement || !date_debut || !lieu || !descr || !duree_evenement) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+  const query = `INSERT INTO Evenements (id_evenement, nom_evenement, lieu, durée_evenement, descr, date_debut) VALUES ('0', '${nom_evenement}', '${lieu}', '${duree_evenement}', '${descr}', '${date_debut}')`;
+  connection.query(query, (error) => {
+    if (error) {
+      throw error;
+    } else {
+      const id_evenement = `SELECT id_evenement FROM Evenements WHERE nom_evenement = '${nom_evenement}' AND lieu = '${lieu}' AND durée_evenement = '${duree_evenement}' AND descr = '${descr}' AND date_debut = '${date_debut}'`;
+      const query2 = `INSERT INTO Organisateurs (id_structure, id_evenement) VALUES ('${structId}', '${id_evenement}')`;
+      connection.query(query2, (error) => {
+        if (error) {
+          const query_del = `DELETE FROM Evenements WHERE nom_evenement = '${nom_evenement}' AND lieu = '${lieu}' AND durée_evenement = '${duree_evenement}' AND descr = '${descr}' AND date_debut = '${date_debut}'`;
+          connection.query(query_del, (error) => {
+            if (error) {
+              throw error;
+            }
+          });
+          throw error;
+        } else {
+          const query3 = `INSERT INTO Participants (id_structure, id_evenement) VALUES ('${structId}', '${id_evenement}')`;
+          connection.query(query3, (error) => {
+            if (error) {
+              const query_del = `DELETE FROM Evenements WHERE nom_evenement = '${nom_evenement}' AND lieu = '${lieu}' AND durée_evenement = '${duree_evenement}' AND descr = '${descr}' AND date_debut = '${date_debut}'`;
+              const query_del2 = `DELETE FROM Organisateurs WHERE id_evenement = '${id_evenement}'`;
+              connection.query(query_del, (error) => {
+                if (error) {
+                  throw error;
+                }
+              });
+              connection.query(query_del2, (error) => {
+                if (error) {
+                  throw error;
+                }
+              });
+              throw error;
+            } else {
+              res.status(201).json({ message: "Event created successfully" });
+            }
+          });
+        }
+      });
+    }
+  });
 };
 
-exports.getEvent = (req, res, next) => {
+exports.getEventStruct = (req, res, next) => {
+  const structId = req.auth.structureId;
+  const query = `SELECT * FROM Evenements WHERE id_evenement IN (SELECT id_evenement FROM Organisateurs WHERE id_structure = '${structId}')`;
+  connection.query(query, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+};
 
+exports.getEventAd = (req, res, next) => {
+  const structId = req.params.structureId;
+  const adherentId = req.auth.adherentId;
+  const query_verif = `SELECT COUNT(*) AS count FROM Participants_Struct WHERE id_structure = '${structId}' AND id_adherent = '${adherentId}'`;
+  connection.query(query_verif, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      if (rows[0].count === 0) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      const query = `SELECT * FROM Evenements WHERE id_evenement IN (SELECT id_evenement FROM Organisateurs WHERE id_structure = '${structId}')`;
+      connection.query(query, (error, rows) => {
+        if (error) {
+          throw error;
+        } else {
+          res.status(200).json(rows);
+        }
+      });
+    }
+  });
+};
+
+exports.joinEvent = (req, res, next) => {
+  const structureId = req.auth.structureId;
+  const id_evenement = parseInt(req.params.id_evenement, 10);
+  const query = `INSERT INTO Participants (id_structure, id_evenement) VALUES ('${structureId}', '${id_evenement}')`;
+  utils.send_query_insert(
+    query,
+    res,
+    200,
+    "Member added to event successfully"
+  );
 };
 
 exports.updateEvent = (req, res, next) => {
-
+  if (
+    !req.body.nom_evenement &&
+    !req.body.lieu &&
+    !req.body.descr &&
+    !req.body.date_debut &&
+    !req.body.duree_evenement
+  ) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+  const structId = req.auth.structureId;
+  const id_evenement = parseInt(req.params.id_evenement, 10);
+  const query_verif = `SELECT COUNT(*) AS count FROM Organisateurs WHERE id_evenement = '${id_evenement}' AND id_structure = '${structId}'`;
+  connection.query(query_verif, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      if (rows[0].count === 0) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      let nom_evenement, lieu, descr, date_debut, duree_evenement;
+      if (req.body.nom_evenement) nom_evenement = req.body.nom_evenement;
+      if (req.body.lieu) lieu = req.body.lieu;
+      if (req.body.descr) descr = req.body.descr;
+      if (req.body.date_debut) date_debut = req.body.date_debut;
+      if (req.body.duree_evenement) duree_evenement = req.body.duree_evenement;
+      let query = `UPDATE Evenements SET `;
+      if (nom_evenement) query += `nom_evenement = '${nom_evenement}', `;
+      if (lieu) query += `lieu = '${lieu}', `;
+      if (descr) query += `descr = '${descr}', `;
+      if (date_debut) query += `date_debut = '${date_debut}', `;
+      if (duree_evenement) query += `duree_evenement = '${duree_evenement}', `;
+      query = query.slice(0, -2);
+      query += ` WHERE id_evenement = '${id_evenement}'`;
+      utils.send_query_insert(query, res, 200, "Event updated successfully");
+    }
+  });
 };
 
 exports.deleteEvent = (req, res, next) => {
-
-};
-
-exports.addParticipant = (req, res, next) => {
-    
+  const structId = req.auth.structureId;
+  const id_evenement = parseInt(req.params.id_evenement, 10);
+  const query_verif = `SELECT COUNT(*) AS count FROM Organisateurs WHERE id_evenement = '${id_evenement}' AND id_structure = '${structId}'`;
+  connection.query(query_verif, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      if (rows[0].count === 0) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      const query = `DELETE FROM Evenements WHERE id_evenement = '${id_evenement}' AND id_evenement IN (SELECT id_evenement FROM Organisateurs WHERE id_structure = '${structId}')`;
+      utils.send_query_insert(query, res, 200, "Event deleted successfully");
+    }
+  });
 };
--- server/routes/event.js
+++ server/routes/event.js
@@ -1,23 +1,93 @@
 // Ce fichier contient les routes concernant les evenements
-
 const express = require("express");
 const router = express.Router();
 const eventCtrl = require("../controllers/event");
-const auth = require("../middleware/auth");
+const auth_struct = require("../middleware/auth_struct");
+const auth_ad = require("../middleware/auth_ad");
 
-/*
-1. **POST** `/events` - Pour créer un nouvel événement.
-2. **GET** `/events/{eventId}` - Pour récupérer les détails d'un événement.
-3. **PUT** `/events/{eventId}` - Pour mettre à jour un événement.
-4. **DELETE** `/events/{eventId}` - Pour supprimer un événement.
-5. **POST** `/events/{eventId}/participants` - Pour ajouter des participants à l'événement.
-*/
+/**
+ * @api {post} /events/create Création d'un événement
+ * @apiName createEvent
+ * @apiGroup Events
+ * 
+ * @apiParam (body) {String} nom_evenement Nom de l'événement
+ * @apiParam (body) {String} date_debut Date de début de l'événement
+ * @apiParam (body) {String} lieu Lieu de l'événement
+ * @apiParam (body) {String} descr Description de l'événement
+ * @apiParam (body) {String} duree_evenement Durée de l'événement
+ * @apiParam (header) {String} Authorization Token d'authentification structure
+ * 
+ * @apiSuccess (201) {String} message Evénement créé avec succès
+ * @apiError (400) {String} message Champs requis manquants
+ * @apiError (500) {Object} error Erreur serveur
+ */
+router.post("/create", auth_struct, eventCtrl.createEvent);
+/**
+ * @api {get} /events/struct/allevents Récupérer tous les événements d'une structure (depuis le token d'authentification structure)
+ * @apiName getEventStruct
+ * @apiGroup Events
+ * 
+ * @apiParam (header) {String} Authorization Token d'authentification structure
+ * 
+ * @apiSuccess (200) {Object[]} events Liste des événements
+ * @apiError (500) {Object} error Erreur serveur
+ */
+router.get("/struct/allevents", auth_struct, eventCtrl.getEventStruct);
+/**
+ * @api {get} /events/ad/:structureId Récupérer tous les événements d'une structure (depuis le token d'authentification adhérent appartenant à la structure)
+ * @apiName getEventAd
+ * @apiGroup Events
+ * 
+ * @apiParam (params) {Number} structureId Id de la structure
+ * @apiParam (header) {String} Authorization Token d'authentification adhérent
+ * 
+ * @apiSuccess (200) {Object[]} events Liste des événements
+ * @apiError (401) {String} message Unauthorized (token ou adherant n'appartient pas a la structure demandée)
+ * @apiError (500) {Object} error Erreur serveur
+ */
+router.get("/ad/:structureId", auth_ad, eventCtrl.getEventAd);
 
-router.post("/", auth, eventCtrl.createEvent);
-router.get("/:eventId",auth,  eventCtrl.getEvent);
-router.put("/:eventId",auth,  eventCtrl.updateEvent);
-router.delete("/:eventId",auth,  eventCtrl.deleteEvent);
-router.post("/:eventId/participants",auth,  eventCtrl.addParticipant);
+/**
+ * @api {post} /events/join/:eventId Rejoindre un événement (depuis le token d'authentification adhérent)
+ * @apiName joinEvent
+ * @apiGroup Events
+ * 
+ * @apiParam (params) {Number} eventId Id de l'événement
+ * @apiParam (header) {String} Authorization Token d'authentification adhérent
+ * 
+ * @apiSuccess (201) {String} message Adhérent inscrit à l'événement
+ * @apiError (401) {String} message Unauthorized (token ou adherant n'appartient pas a la structure demandée)
+ */
+router.post("/join/:eventId", auth_struct, eventCtrl.joinEvent);
+
+/**
+ * @api {put} /events/update/:eventId Modifier un événement (depuis le token d'authentification structure)
+ * @apiName updateEvent
+ * @apiGroup Events
+ * 
+ * @apiParam (params) {Number} eventId Id de l'événement
+ * @apiParam (body) {String} nom_evenement Nouveau nom de l'événement (optionnel)
+ * @apiParam (body) {String} date_debut Nouvelle date de début de l'événement (optionnel)
+ * @apiParam (body) {String} lieu Nouveau lieu de l'événement (optionnel)
+ * @apiParam (body) {String} descr Nouvelle description de l'événement (optionnel)
+ * @apiParam (body) {String} duree_evenement Nouvelle durée de l'événement (optionnel)
+ * @apiParam (header) {String} Authorization Token d'authentification structure
+ * 
+ */
+router.put("/update/:eventId", auth_struct, eventCtrl.updateEvent);
+
+/**
+ * @api {delete} /events/delete/:eventId Supprimer un événement (depuis le token d'authentification structure)
+ * @apiName deleteEvent
+ * @apiGroup Events
+ * 
+ * @apiParam (params) {Number} eventId Id de l'événement
+ * @apiParam (header) {String} Authorization Token d'authentification structure
+ * 
+ * @apiSuccess (200) {String} message Evénement supprimé avec succès
+ * @apiError (401) {String} message Unauthorized (token ou adherant n'appartient pas a la structure demandée)
+ */
+router.delete("/delete/:eventId", auth_struct, eventCtrl.deleteEvent);
 
 router.use((req, res, next) => {
   console.log("Requete d'evenement");
