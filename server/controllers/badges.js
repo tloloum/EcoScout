bbb785ed705968b8a77120aa83941cb1ad0d6189
Arthur <arthurdurand3@gmail.com>
2024-05-10 17:45:05 +0200

    avancé sur les badges

--- server/controllers/badges.js
+++ server/controllers/badges.js
@@ -1,43 +1,76 @@
 const connection = require("../bdd/utils/connection.js");
 const utils = require("../bdd/utils/utils");
 
+// exports.createBadges = (req, res, next) => {
+//   const structId = req.params.structureId;
+//   if (req.auth.structureId != structId) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+//   const query = `SELECT * FROM Adherents WHERE Adherents.id_structure= '${structId}'`;
+//   connection.query(query, (error, rows) => {
+//     if (error) {
+//       throw error;
+//     } else {
+//       rows.forEach((row) => {
+//         const query_badge = `INSERT INTO Badges (id_adherent, id_structure, statut) VALUES ('${row.id_adherent}', '${structId}', FALSE)`;
+//         connection.query(query_badge, (error) => {
+//           if (error) {
+//             throw error;
+//           }
+//         });
+//       });
+//       res.status(200).json({ message: "Badges created" });
+//     }
+//   });
+// };
+
 exports.getBadgesFromAd = (req, res, next) => {
-  // if (!req.auth.adherentId) {
-  //   return res.status(401).json({ message: "Unauthorized" });
-  // }
-  // const adherentId = req.auth.adherentId;
-  // const query_verif = `SELECT * FROM Badges WHERE Badges.id_adherent= '${adherentId}'`;
-  // connection.query(query, (error, rows) => {
-  //   if (error) {
-  //     throw error;
-  //   } else {
-  //     res.status(200).json(rows);
-  //   }
-  // });
-  //TODO
+  const adherendId = parseInt(req.params.adherentId, 10);
+  if (req.auth.adherentId != adherendId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const query = `SELECT * FROM Badges WHERE Badges.id_adherent= '${adherendId}'`;
+  connection.query(query, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json(rows);
+    }
+  });
 };
 
 exports.getBadgesFromStruct = (req, res, next) => {
+  const structId = req.params.structureId;
+  if (req.auth.structureId != structId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
   // if (req.body.structureId == undefined) {
   //   return res.status(400).json({ message: "Missing required fields" });
   // }
-  // const structId = req.params.structureId;
-  // const query_verif = `SELECT * FROM Badges WHERE Badges.id_structure= '${structId}'`;
-  // connection.query(query, (error, rows) => {
-  //   if (error) {
-  //     throw error;
-  //   } else {
-  //     res.status(200).json(rows);
-  //   }
-  // });
-  //TODO
+  const query_verif = `SELECT * FROM Badges WHERE Badges.id_structure= '${structId}'`;
+  connection.query(query, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json(rows);
+    }
+  });
 };
 
 exports.updateBadgesFromAd = (req, res, next) => {
-  // if (!req.auth.adherentId) {
-  //   return res.status(401).json({ message: "Unauthorized" });
-  // }
-  //TODO
+  const adherendId = parseInt(req.params.adherentId, 10);
+  if (req.auth.adherentId != adherendId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const badgeId = parseInt(req.params.id_badges, 10);
+  const query = `UPDATE Badges SET statut = TRUE WHERE Badges.id_badges = '${badgeId}' AND Badges.id_adherent = '${adherendId}'`;
+  connection.query(query, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json({ message: "Badge updated" });
+    }
+  });
 };
 
 exports.updateBadgesFromStruct = (req, res, next) => {
--- server/routes/badges.js
+++ server/routes/badges.js
@@ -6,22 +6,28 @@ const badgesCtrl = require("../controllers/badges");
 const auth_ad = require("../middleware/auth_ad");
 const auth_struct = require("../middleware/auth_struct");
 
+// // génère les badges pour une structure
+// router.post("/create/:structureId", auth_struct, badgesCtrl.createBadges);
+
+// // génère les badges pour un adhérent
+// router.post("/create/adherent/:adherentId", auth_ad, badgesCtrl.createBadges);
+
 // Récupération de tous les badges possible pour un adhérent
-router.get("/badges/adherent/:adherentId", auth_ad, badgesCtrl.getBadgesFromAd);
+router.get("/adherent/:adherentId", auth_ad, badgesCtrl.getBadgesFromAd);
 
 // Récupération de tous les badges possible pour une structure
-router.get("/badges/:structureId", auth_struct, badgesCtrl.getBadgesFromStruct);
+router.get("/:structureId", auth_struct, badgesCtrl.getBadgesFromStruct);
 
 // Validation d'un badge pour un adhérent
 router.put(
-  "/update/adherent/:id_badges",
+  "/update/:id_badges/adherent/:adherentId",
   auth_ad,
   badgesCtrl.updateBadgesFromAd
 );
 
 // Validation d'un badge pour une structure
 router.put(
-  "/update/struct/:id_badges",
+  "/update/:id_badges/struct/:structureId",
   auth_struct,
   badgesCtrl.updateBadgesFromStruct
 );
