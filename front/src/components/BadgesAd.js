b87d272bea80989e3413c0447afb1c60ac3a7ce9
MKADEM Malek <Malek.Mkadem@bordeaux-inp.fr>
2024-05-10 14:16:12 +0200

    debut badges

--- front/src/components/BadgesAd.js
+++ front/src/components/BadgesAd.js
@@ -1,12 +1,44 @@
 import React from "react";
+import badge1 from "../assets/img/badge.jpeg";
 
 const BadgesAd = () => {
   const PossibleBadgesAd = [
-    { name: "Badge 1", description: "Description du badge 1" },
-    { name: "Badge 2", description: "Description du badge 2" },
-    { name: "Badge 3", description: "Description du badge 3" },
+    {
+      name: "Badge 1",
+      description: "Avoir participé à 3 évènements",
+      src: badge1,
+    },
+    {
+      name: "Badge 2",
+      description: "condition d'obtention du badge 2",
+      src: "",
+    },
+    {
+      name: "Badge 3",
+      description: "condition d'obtention du badge 3",
+      src: "",
+    },
+    {
+      name: "Badge 4",
+      description: "condition d'obtention du badge ",
+      src: "",
+    },
   ];
-  return <div></div>;
+
+  return (
+    <div>
+      <div className="badges-list">
+        {PossibleBadgesAd.map((badge, index) => (
+          <div key={index} className="badge-item">
+            <img src={badge.src} alt={badge.name} />
+            <div className="badge-details">
+              <h4>{badge.description}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BadgesAd;
