4b7545723c1b4157f736bcbcf12a4b6be94c4287
Arthur <arthurdurand3@gmail.com>
2024-05-14 11:06:31 +0200

    ajout de la liste et des admins et des suppr

--- front/src/pages/ListAdStruct.js
+++ front/src/pages/ListAdStruct.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ListOfParticipants from '../components/ListOfParticipants';
+import Sidebar from "../components/Sidebar";
+
 
 const ListAdStruct = () => {
     return (
     <div>
+        <Sidebar/>
         <ListOfParticipants/>
         
     </div>
